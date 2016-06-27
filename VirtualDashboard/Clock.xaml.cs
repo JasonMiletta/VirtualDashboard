ed43a54676ca8ec0dc39b9136103cb40067949e7
Jason Miletta <jmiletta01@gmail.com>
2016-06-26 20:04:51 -0500

    Add clock component and add to main page

---
+++ VirtualDashboard/Clock.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace VirtualDashboard
+{
+    /// <summary>
+    /// Clock Component
+    /// </summary>
+    public sealed partial class Clock : Page
+    {
+        public Clock()
+        {
+            this.InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            timer.Tick += new EventHandler<object>(timer_Tick);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, object e)
+        {
+            clockText.Text = DateTime.Now.ToString("h:mm:ss tt");
+        }
+    }
+}
