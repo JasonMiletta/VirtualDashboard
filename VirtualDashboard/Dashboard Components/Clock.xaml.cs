af2ad509b2b6decdae0790eb2993708d25bc4395
Jason Miletta <jmiletta01@gmail.com>
2016-06-28 16:47:43 -0500

    Dev (#1)
    * Add basic buttons
    
    * Add clock component and add to main page
    
    * Fixed App's Packagename when deploying
    
    * Create and Add Date component
    
    * Add Google Calendar API package and google OAuth2 client id

---
+++ VirtualDashboard/Dashboard Components/Clock.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace VirtualDashboard
+{
+    /// <summary>
+    /// Clock Component
+    /// </summary>
+    public sealed partial class Clock : Page
+    {
+        public Clock()
+        {
+            this.InitializeComponent();
+            setupClockDispatcherTimer();
+        }
+
+        private void setupClockDispatcherTimer()
+        {
+            clockText.Text = DateTime.Now.ToString("h:mm:ss tt");
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Tick += new EventHandler<object>(timer_Tick);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, object e)
+        {
+            clockText.Text = DateTime.Now.ToString("h:mm:ss tt");
+        }
+    }
+}
